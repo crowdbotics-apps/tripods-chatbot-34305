6bca4d3d6797cf5cda60f6288e5e3d25ae370f85
Crowdbotics <team@crowdbotics.com>
2022-03-19 14:59:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tripods_chatbot_34305",
+  url: "https://tripods_chatbot_34305.botics.co",
+  api: "https://tripods_chatbot_34305.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
